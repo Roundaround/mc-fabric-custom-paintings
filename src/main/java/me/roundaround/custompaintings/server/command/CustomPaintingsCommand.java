ca0e772823a6349a2472204cccac1ac4cfb94a3e
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-12-30 13:51:26 -0500

    Added locatemissing command

--- src/main/java/me/roundaround/custompaintings/server/command/CustomPaintingsCommand.java
+++ src/main/java/me/roundaround/custompaintings/server/command/CustomPaintingsCommand.java
@@ -1,5 +1,8 @@
 package me.roundaround.custompaintings.server.command;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -7,6 +10,8 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 import me.roundaround.custompaintings.CustomPaintingsMod;
+import me.roundaround.custompaintings.entity.decoration.painting.ExpandedPaintingEntity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -25,8 +30,15 @@ public class CustomPaintingsCommand {
           return executeListKnown(context.getSource());
         });
 
+    LiteralArgumentBuilder<ServerCommandSource> locateMissingSub = CommandManager
+        .literal("locatemissing")
+        .executes(context -> {
+          return executeLocateMissing(context.getSource());
+        });
+
     LiteralArgumentBuilder<ServerCommandSource> finalCommand = baseCommand
-        .then(listKnownSub);
+        .then(listKnownSub)
+        .then(locateMissingSub);
 
     dispatcher.register(finalCommand);
   }
@@ -40,12 +52,43 @@ public class CustomPaintingsCommand {
       return 0;
     }
 
-    String ids = CustomPaintingsMod.knownPaintings
+    List<String> ids = CustomPaintingsMod.knownPaintings
         .get(uuid)
         .stream()
         .map(Identifier::toString)
-        .collect(Collectors.joining(", "));
-    source.sendFeedback(Text.literal(ids), true);
-    return 1;
+        .collect(Collectors.toList());
+
+    if (ids.size() == 0) {
+      source.sendFeedback(Text.translatable("command.custompaintings.listknown.none"), true);
+    } else {
+      source.sendFeedback(Text.literal(String.join(", ", ids)), true);
+    }
+
+    return ids.size();
+  }
+
+  private static int executeLocateMissing(ServerCommandSource source) {
+    ServerPlayerEntity player = source.getPlayer();
+    UUID uuid = player.getUuid();
+    HashSet<Identifier> known = CustomPaintingsMod.knownPaintings.get(uuid);
+    ArrayList<String> missing = new ArrayList<>();
+
+    source.getServer().getWorlds().forEach((world) -> {
+      world.getEntitiesByType(EntityType.PAINTING, entity -> entity instanceof ExpandedPaintingEntity)
+          .stream()
+          .map((entity) -> ((ExpandedPaintingEntity) entity).getCustomData().id())
+          .filter((id) -> !known.contains(id))
+          .forEach((id) -> {
+            missing.add(id.toString());
+          });
+    });
+
+    if (missing.isEmpty()) {
+      source.sendFeedback(Text.translatable("command.custompaintings.locatemissing.none"), true);
+    } else {
+      source.sendFeedback(Text.literal(String.join(", ", missing)), true);
+    }
+
+    return missing.size();
   }
 }
