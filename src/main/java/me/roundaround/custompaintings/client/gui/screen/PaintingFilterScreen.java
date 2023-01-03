0938f0f6bd6edc77c08c9f53d21eee5da46f8188
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-01-02 23:10:21 -0500

    Render background in filter page

--- src/main/java/me/roundaround/custompaintings/client/gui/screen/PaintingFilterScreen.java
+++ src/main/java/me/roundaround/custompaintings/client/gui/screen/PaintingFilterScreen.java
@@ -1,7 +1,15 @@
 package me.roundaround.custompaintings.client.gui.screen;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
 public class PaintingFilterScreen extends Screen {
@@ -28,5 +36,42 @@ public class PaintingFilterScreen extends Screen {
         });
 
     addDrawableChild(closeButton);
+  }
+
+  @Override
+  public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+    renderBackgroundInRegion(0, height, 0, width);
+    super.render(matrixStack, mouseX, mouseY, partialTicks);
+  }
+
+  private void renderBackgroundInRegion(int top, int bottom, int left, int right) {
+    Tessellator tessellator = Tessellator.getInstance();
+    BufferBuilder bufferBuilder = tessellator.getBuffer();
+    RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
+    RenderSystem.setShaderTexture(0, OPTIONS_BACKGROUND_TEXTURE);
+    RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+
+    bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
+    bufferBuilder
+        .vertex(left, bottom, 0)
+        .texture(left / 32f, bottom / 32f)
+        .color(64, 64, 64, 255)
+        .next();
+    bufferBuilder
+        .vertex(right, bottom, 0)
+        .texture(right / 32f, bottom / 32f)
+        .color(64, 64, 64, 255)
+        .next();
+    bufferBuilder
+        .vertex(right, top, 0)
+        .texture(right / 32f, top / 32f)
+        .color(64, 64, 64, 255)
+        .next();
+    bufferBuilder
+        .vertex(left, top, 0)
+        .texture(left / 32f, top / 32f)
+        .color(64, 64, 64, 255)
+        .next();
+    tessellator.draw();
   }
 }
