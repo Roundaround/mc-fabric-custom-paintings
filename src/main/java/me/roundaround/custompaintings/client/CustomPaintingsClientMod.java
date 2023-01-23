ab36ea88e3d0369ab386beb1dd85aaa736d7fe49
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-01-23 10:16:47 -0500

    Add keybinding to open management screen

--- src/main/java/me/roundaround/custompaintings/client/CustomPaintingsClientMod.java
+++ src/main/java/me/roundaround/custompaintings/client/CustomPaintingsClientMod.java
@@ -1,21 +1,28 @@
 package me.roundaround.custompaintings.client;
 
+import org.lwjgl.glfw.GLFW;
+
 import me.roundaround.custompaintings.client.event.MinecraftClientEvents;
+import me.roundaround.custompaintings.client.gui.screen.manage.ManagePaintingsScreen;
 import me.roundaround.custompaintings.client.network.ClientNetworking;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.resource.ResourceType;
 
 public class CustomPaintingsClientMod implements ClientModInitializer {
   public static CustomPaintingManager customPaintingManager;
+  public static KeyBinding openManageScreenKeyBinding;
 
   @Override
   public void onInitializeClient() {
     MinecraftClientEvents.AFTER_INIT.register(() -> {
-      MinecraftClient minecraft = MinecraftClient.getInstance();
-      customPaintingManager = new CustomPaintingManager(minecraft.getTextureManager());
+      MinecraftClient client = MinecraftClient.getInstance();
+      customPaintingManager = new CustomPaintingManager(client.getTextureManager());
       ResourceManagerHelper
           .get(ResourceType.CLIENT_RESOURCES)
           .registerReloadListener(customPaintingManager);
@@ -27,5 +34,23 @@ public class CustomPaintingsClientMod implements ClientModInitializer {
     });
 
     ClientNetworking.registerReceivers();
+
+    openManageScreenKeyBinding = KeyBindingHelper.registerKeyBinding(
+        new KeyBinding(
+            "custompaintings.key.open_manage_screen",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_K,
+            "key.categories.misc"));
+
+    MinecraftClientEvents.ON_INPUT.register(() -> {
+      MinecraftClient client = MinecraftClient.getInstance();
+      if (client.currentScreen != null) {
+        return;
+      }
+
+      if (openManageScreenKeyBinding.wasPressed()) {
+        client.setScreen(new ManagePaintingsScreen());
+      }
+    });
   }
 }
