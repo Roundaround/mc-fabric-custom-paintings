c24c0f96e96b7794ba7a43a79d8e4eb515e1d30a
Roundaround <roundaroundna@gmail.com>
2024-06-21 14:03:50 -0400

    Use default button width & height

--- src/main/java/me/roundaround/custompaintings/client/gui/screen/edit/GroupSelectScreen.java
+++ src/main/java/me/roundaround/custompaintings/client/gui/screen/edit/GroupSelectScreen.java
@@ -12,9 +12,6 @@ import net.minecraft.text.Text;
 import java.util.Objects;
 
 public class GroupSelectScreen extends PaintingEditScreen {
-  private static final int FOOTER_BUTTON_WIDTH = 200;
-  private static final int FOOTER_BUTTON_HEIGHT = 20;
-
   protected final ThreePartsLayoutWidget layout = new ThreePartsLayoutWidget(this);
 
   public GroupSelectScreen(PaintingEditState state) {
@@ -29,8 +26,7 @@ public class GroupSelectScreen extends PaintingEditScreen {
     groupsListWidget.setGroups(this.state.getGroups());
     this.layout.addBody(groupsListWidget);
 
-    this.layout.addFooter(
-        ButtonWidget.builder(ScreenTexts.CANCEL, this::close).size(FOOTER_BUTTON_WIDTH, FOOTER_BUTTON_HEIGHT).build());
+    this.layout.addFooter(ButtonWidget.builder(ScreenTexts.CANCEL, this::close).build());
   }
 
   @Override
