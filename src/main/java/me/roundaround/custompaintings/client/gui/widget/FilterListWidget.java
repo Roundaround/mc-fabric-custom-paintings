48a34defe03fa92c65ce3ad32677a1f581e34adc
Roundaround <roundaroundna@gmail.com>
2024-06-21 12:44:06 -0400

    Reordered FilterListWidget.Entry constructor parameters

--- src/main/java/me/roundaround/custompaintings/client/gui/widget/FilterListWidget.java
+++ src/main/java/me/roundaround/custompaintings/client/gui/widget/FilterListWidget.java
@@ -27,47 +27,47 @@ public class FilterListWidget extends FlowListWidget<FilterListWidget.Entry> {
   ) {
     super(client, layout.getX(), layout.getHeaderHeight(), layout.getWidth(), layout.getContentHeight());
 
-    this.addEntry((index, left, top, width) -> new SectionTitleEntry(index, left, top, width, this.client.textRenderer,
-        Text.translatable("custompaintings.filter.section.search")
+    this.addEntry((index, left, top, width) -> new SectionTitleEntry(this.client.textRenderer,
+        Text.translatable("custompaintings.filter.section.search"), index, left, top, width
     ));
 
-    this.addEntry((index, left, top, width) -> new TextFilterEntry(index, left, top, width, this.client.textRenderer,
+    this.addEntry((index, left, top, width) -> new TextFilterEntry(this.client.textRenderer,
         Text.translatable("custompaintings.filter.any"), () -> state.getFilters().getSearch(),
-        (value) -> state.getFilters().setSearch(value)
+        (value) -> state.getFilters().setSearch(value), index, left, top, width
     ));
 
-    this.addEntry((index, left, top, width) -> new TextFilterEntry(index, left, top, width, this.client.textRenderer,
+    this.addEntry((index, left, top, width) -> new TextFilterEntry(this.client.textRenderer,
         Text.translatable("custompaintings.filter.name"), () -> state.getFilters().getNameSearch(),
-        (value) -> state.getFilters().setNameSearch(value)
+        (value) -> state.getFilters().setNameSearch(value), index, left, top, width
     ));
 
-    this.addEntry((index, left, top, width) -> new TextFilterEntry(index, left, top, width, this.client.textRenderer,
+    this.addEntry((index, left, top, width) -> new TextFilterEntry(this.client.textRenderer,
         Text.translatable("custompaintings.filter.artist"), () -> state.getFilters().getArtistSearch(),
-        (value) -> state.getFilters().setArtistSearch(value)
+        (value) -> state.getFilters().setArtistSearch(value), index, left, top, width
     ));
 
     // TODO: Name/artist is empty
 
-    this.addEntry((index, left, top, width) -> new SectionTitleEntry(index, left, top, width, this.client.textRenderer,
-        Text.translatable("custompaintings.filter.section.size")
-    ));
-
-    this.addEntry((index, left, top, width) -> new ToggleFilterEntry(index, left, top, width,
-        Text.translatable("custompaintings.filter.canstay"), ScreenTexts.ON, ScreenTexts.OFF,
-        () -> state.getFilters().getCanStayOnly(), (value) -> state.getFilters().setCanStayOnly(value)
+    this.addEntry((index, left, top, width) -> new SectionTitleEntry(this.client.textRenderer,
+        Text.translatable("custompaintings.filter.section.size"), index, left, top, width
     ));
 
     this.addEntry(
-        (index, left, top, width) -> new SizeRangeEntry(index, left, top, width, () -> state.getFilters().getMinWidth(),
-            () -> state.getFilters().getMaxWidth(), (value) -> state.getFilters().setMinWidth(value),
-            (value) -> state.getFilters().setMaxWidth(value), "custompaintings.filter.minwidth",
-            "custompaintings.filter.maxwidth"
+        (index, left, top, width) -> new ToggleFilterEntry(Text.translatable("custompaintings.filter.canstay"),
+            ScreenTexts.ON, ScreenTexts.OFF, () -> state.getFilters().getCanStayOnly(),
+            (value) -> state.getFilters().setCanStayOnly(value), index, left, top, width
         ));
 
-    this.addEntry((index, left, top, width) -> new SizeRangeEntry(index, left, top, width,
-        () -> state.getFilters().getMinHeight(), () -> state.getFilters().getMaxHeight(),
-        (value) -> state.getFilters().setMinHeight(value), (value) -> state.getFilters().setMaxHeight(value),
-        "custompaintings.filter.minheight", "custompaintings.filter.maxheight"
+    this.addEntry((index, left, top, width) -> new SizeRangeEntry(() -> state.getFilters().getMinWidth(),
+        () -> state.getFilters().getMaxWidth(), (value) -> state.getFilters().setMinWidth(value),
+        (value) -> state.getFilters().setMaxWidth(value), "custompaintings.filter.minwidth",
+        "custompaintings.filter.maxwidth", index, left, top, width
+    ));
+
+    this.addEntry((index, left, top, width) -> new SizeRangeEntry(() -> state.getFilters().getMinHeight(),
+        () -> state.getFilters().getMaxHeight(), (value) -> state.getFilters().setMinHeight(value),
+        (value) -> state.getFilters().setMaxHeight(value), "custompaintings.filter.minheight",
+        "custompaintings.filter.maxheight", index, left, top, width
     ));
   }
 
@@ -112,7 +112,7 @@ public class FilterListWidget extends FlowListWidget<FilterListWidget.Entry> {
   public static class SectionTitleEntry extends Entry {
     protected final LabelWidget label;
 
-    public SectionTitleEntry(int index, int left, int top, int width, TextRenderer textRenderer, Text label) {
+    public SectionTitleEntry(TextRenderer textRenderer, Text label, int index, int left, int top, int width) {
       super(index, left, top, width, HEIGHT);
 
       this.label = LabelWidget.builder(textRenderer, label)
@@ -142,15 +142,15 @@ public class FilterListWidget extends FlowListWidget<FilterListWidget.Entry> {
     private final CyclingButtonWidget<Boolean> button;
 
     public ToggleFilterEntry(
-        int index,
-        int left,
-        int top,
-        int width,
         Text label,
         Text trueText,
         Text falseText,
         Supplier<Boolean> getter,
-        Consumer<Boolean> setter
+        Consumer<Boolean> setter,
+        int index,
+        int left,
+        int top,
+        int width
     ) {
       super(index, left, top, width, HEIGHT);
 
@@ -190,14 +190,14 @@ public class FilterListWidget extends FlowListWidget<FilterListWidget.Entry> {
     private final TextFieldWidget textField;
 
     public TextFilterEntry(
-        int index,
-        int left,
-        int top,
-        int width,
         TextRenderer textRenderer,
         Text label,
         Supplier<String> getter,
-        Consumer<String> setter
+        Consumer<String> setter,
+        int index,
+        int left,
+        int top,
+        int width
     ) {
       super(index, left, top, width, HEIGHT);
 
@@ -250,16 +250,16 @@ public class FilterListWidget extends FlowListWidget<FilterListWidget.Entry> {
     private final Consumer<Integer> highSetter;
 
     public SizeRangeEntry(
-        int index,
-        int left,
-        int top,
-        int width,
         Supplier<Integer> lowGetter,
         Supplier<Integer> highGetter,
         Consumer<Integer> lowSetter,
         Consumer<Integer> highSetter,
         String lowI18nKey,
-        String highI18nKey
+        String highI18nKey,
+        int index,
+        int left,
+        int top,
+        int width
     ) {
       super(index, left, top, width, HEIGHT);
 
