83debbeeb8b86f71c14f99e75e88d5798666e53f
Roundaround <roundaroundna@gmail.com>
2024-07-09 11:30:04 -0400

    Prefer WrapOperation over Redirect

--- src/main/java/me/roundaround/custompaintings/mixin/DecorationItemMixin.java
+++ src/main/java/me/roundaround/custompaintings/mixin/DecorationItemMixin.java
@@ -1,40 +1,53 @@
 package me.roundaround.custompaintings.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import me.roundaround.custompaintings.server.network.ServerNetworking;
 import net.minecraft.entity.decoration.painting.PaintingEntity;
+import net.minecraft.entity.decoration.painting.PaintingVariant;
 import net.minecraft.entity.decoration.painting.PaintingVariants;
 import net.minecraft.item.DecorationItem;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Optional;
 
 @Mixin(DecorationItem.class)
 public abstract class DecorationItemMixin {
-  @Redirect(
+  @WrapOperation(
       method = "useOnBlock", at = @At(
       value = "INVOKE",
-      target = "net/minecraft/entity/decoration/painting/PaintingEntity.placePainting(Lnet/minecraft/world/World;" +
-          "Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)Ljava/util/Optional;"
+      target = "Lnet/minecraft/entity/decoration/painting/PaintingEntity;placePainting" +
+          "(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)" +
+          "Ljava/util/Optional;"
   )
   )
-  private Optional<PaintingEntity> placePainting(
-      World world, BlockPos pos, Direction facing, ItemUsageContext context
+  private Optional<PaintingEntity> wrapPlacePainting(
+      World world,
+      BlockPos pos,
+      Direction facing,
+      Operation<Optional<PaintingEntity>> original,
+      @Local(argsOnly = true) ItemUsageContext context
   ) {
     if (!(context.getPlayer() instanceof ServerPlayerEntity player)) {
-      return PaintingEntity.placePainting(world, pos, facing);
+      return original.call(world, pos, facing);
     }
 
-    PaintingEntity painting = new PaintingEntity(world, pos, facing,
-        Registries.PAINTING_VARIANT.getEntry(PaintingVariants.KEBAB).get()
-    );
+    Optional<RegistryEntry.Reference<PaintingVariant>> placeholderVariant = Registries.PAINTING_VARIANT.getEntry(
+        PaintingVariants.KEBAB);
+    if (placeholderVariant.isEmpty()) {
+      return Optional.empty();
+    }
+
+    PaintingEntity painting = new PaintingEntity(world, pos, facing, placeholderVariant.get());
 
     if (!painting.canStayAttached()) {
       return Optional.empty();
