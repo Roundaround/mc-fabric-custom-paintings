e3904b503dc9379b09f4b3fb076f5d4bdad32ba4
Evan Steinkerchner <esteinkerchner@gmail.com>
2024-10-30 17:30:14 -0400

    Skip loading painting packs if in safe mode

--- src/main/java/me/roundaround/custompaintings/mixin/SaveLoadingDataPacksMixin.java
+++ src/main/java/me/roundaround/custompaintings/mixin/SaveLoadingDataPacksMixin.java
@@ -5,15 +5,24 @@ import me.roundaround.custompaintings.server.registry.ServerPaintingRegistry;
 import net.minecraft.resource.DataConfiguration;
 import net.minecraft.resource.LifecycledResourceManager;
 import net.minecraft.server.SaveLoading;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SaveLoading.DataPacks.class)
 public abstract class SaveLoadingDataPacksMixin {
+  @Shadow
+  @Final
+  private boolean safeMode;
+
   @Inject(method = "load", at = @At(value = "RETURN"))
   private void onLoad(CallbackInfoReturnable<Pair<DataConfiguration, LifecycledResourceManager>> cir) {
+    if (this.safeMode) {
+      return;
+    }
     ServerPaintingRegistry.getInstance().firstLoadPaintingPacks();
   }
 }
