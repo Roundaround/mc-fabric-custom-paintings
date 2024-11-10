0315dc31efa1aba32c98cec90f9ffa1976850283
Evan Steinkerchner <esteinkerchner@gmail.com>
2024-11-10 16:49:43 -0500

    Remove unused import

--- src/main/java/me/roundaround/custompaintings/client/CustomPaintingsClientMod.java
+++ src/main/java/me/roundaround/custompaintings/client/CustomPaintingsClientMod.java
@@ -1,10 +1,8 @@
 package me.roundaround.custompaintings.client;
 
-import me.roundaround.custompaintings.client.gui.screen.MainMenuScreen;
 import me.roundaround.custompaintings.client.network.ClientNetworking;
 import me.roundaround.custompaintings.client.option.KeyBindings;
 import me.roundaround.custompaintings.client.registry.ClientPaintingRegistry;
-import me.roundaround.roundalib.client.event.MinecraftClientEvents;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
