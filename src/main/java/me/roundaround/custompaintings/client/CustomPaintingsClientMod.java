29ebafff8c68dfd9325aaae24f230e1212d8024b
Evan Steinkerchner <esteinkerchner@gmail.com>
2024-11-10 16:47:57 -0500

    Add keybinding to open main menu screen

--- src/main/java/me/roundaround/custompaintings/client/CustomPaintingsClientMod.java
+++ src/main/java/me/roundaround/custompaintings/client/CustomPaintingsClientMod.java
@@ -1,7 +1,10 @@
 package me.roundaround.custompaintings.client;
 
+import me.roundaround.custompaintings.client.gui.screen.MainMenuScreen;
 import me.roundaround.custompaintings.client.network.ClientNetworking;
+import me.roundaround.custompaintings.client.option.KeyBindings;
 import me.roundaround.custompaintings.client.registry.ClientPaintingRegistry;
+import me.roundaround.roundalib.client.event.MinecraftClientEvents;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
@@ -9,6 +12,8 @@ public class CustomPaintingsClientMod implements ClientModInitializer {
   @Override
   public void onInitializeClient() {
     ClientNetworking.registerReceivers();
+
+    KeyBindings.register();
 
     ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
       ClientPaintingRegistry.getInstance().clear();
---
+++ src/main/java/me/roundaround/custompaintings/client/option/KeyBindings.java
@@ -0,0 +1,44 @@
+package me.roundaround.custompaintings.client.option;
+
+import me.roundaround.custompaintings.client.gui.screen.MainMenuScreen;
+import me.roundaround.roundalib.client.event.MinecraftClientEvents;
+import me.roundaround.roundalib.client.event.ScreenInputEvent;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBindings {
+  public static KeyBinding openMenu;
+
+  private KeyBindings() {
+  }
+
+  public static void register() {
+    // TODO: i18n
+    openMenu = KeyBindingHelper.registerKeyBinding(
+        new KeyBinding("custompaintings.key.openMainMenu", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_U,
+            "custompaintings.keyCategory"
+        ));
+
+    MinecraftClientEvents.HANDLE_INPUT.register((client) -> {
+      while (openMenu.wasPressed()) {
+        client.setScreen(new MainMenuScreen(client.currentScreen));
+      }
+    });
+
+    ScreenInputEvent.EVENT.register((screen, keyCode, scanCode, modifiers) -> {
+      MinecraftClient client = MinecraftClient.getInstance();
+      if (client.currentScreen != null && !(client.currentScreen instanceof TitleScreen)) {
+        return false;
+      }
+      if (openMenu.matchesKey(keyCode, scanCode)) {
+        client.setScreen(new MainMenuScreen(client.currentScreen));
+        return true;
+      }
+      return false;
+    });
+  }
+}
